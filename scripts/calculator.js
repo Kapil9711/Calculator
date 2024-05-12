9898fc560291ab34f8c7a4a59aa10153d1d201d1
kapil <kapil.personal9711@gmail.com>
2024-05-12 13:44:47 +0530

    initial handling result

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -6,7 +6,7 @@ const input = document.querySelector("input");
 const validateInput = (value) => {
   const inputValue = input.value;
   if (!inputValue.length) return value === "." || !!Number(value);
-  if (!Number(inputValue[inputValue.length - 1]) && value != ".")
+  if (!Number(inputValue[inputValue.length - 1]) && value !== ".")
     return !!Number(value);
   if (value === ".")
     return !inputValue
@@ -15,12 +15,10 @@ const validateInput = (value) => {
 
   return true;
 };
+
 const handleInput = (innerText) => {
   let value = innerText;
-  if (!Number(innerText) && !(innerText === ".")) {
-    console.log("h");
-    value = " " + value + " ";
-  }
+  if (!Number(innerText) && !(innerText === ".")) value = " " + value + " ";
   validateInput(value) ? (input.value += value) : "";
 };
 
@@ -29,10 +27,43 @@ const handleClear = (innerText) => {
   input.value = input.value.slice(0, input.value.length - 1);
 };
 
+const handleResult = () => {
+  const inputArr = input.value
+    .split(" ")
+    .map((ele) => (Number(ele) ? Number(ele) : ele));
+  let result;
+  let indexofDiv = inputArr.indexOf("/");
+  if (indexofDiv !== -1) {
+    result = inputArr[indexofDiv - 1] / inputArr[indexofDiv + 1];
+    inputArr.splice(indexofDiv - 1, 3, result);
+  }
+  let indexofMul = inputArr.indexOf("*");
+
+  if (indexofMul !== -1) {
+    result = inputArr[indexofMul - 1] * inputArr[indexofMul + 1];
+    inputArr.splice(indexofMul - 1, 3, result);
+  }
+  let indexofAdd = inputArr.indexOf("+");
+
+  if (indexofAdd !== -1) {
+    result = inputArr[indexofAdd - 1] + inputArr[indexofAdd + 1];
+    inputArr.splice(indexofAdd - 1, 3, result);
+  }
+  let indexofSub = inputArr.indexOf("-");
+
+  if (indexofSub !== -1) {
+    result = inputArr[indexofSub - 1] - inputArr[indexofSub + 1];
+    inputArr.splice(indexofSub - 1, 3, result);
+  }
+
+  input.value = inputArr[0];
+};
+
 [...numbers, ...mathOperation, ...operations].forEach((btn) => {
   btn.addEventListener("click", function () {
     const innerText = this.innerText;
     if (innerText === "Ac" || innerText === "X") return handleClear(innerText);
+    if (innerText === "=") return handleResult();
     handleInput(innerText);
   });
 });
