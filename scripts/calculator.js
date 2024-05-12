edad88cd90af298bcb73dc6277a03e119449be4e
kapil <kapil.personal9711@gmail.com>
2024-05-12 10:23:33 +0530

    selected input element

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,3 +1,4 @@
 const operations = document.querySelectorAll(".operation");
 const mathOperation = document.querySelectorAll(".math-operation");
 const numbers = document.querySelectorAll(".number");
+const input = document.querySelector("input");
